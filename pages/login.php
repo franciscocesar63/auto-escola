e4e1dbde33fb53472479a91fb03e7d9504bcf8dd
Francisco César <franciscocesar888@gmail.com>
2019-07-04 21:32:33 -0300

    Login page

--- pages/login.php
+++ pages/login.php
@@ -4,9 +4,24 @@ include_once '../config/config.php';
 include_once DIRREQ . 'header.php';
 ?>
 
-<div class="bg-primary">
-    
-    CORPO
+<div class="container">
+
+    <form class="mt-5" style="text-align: center">
+        <img class="mb-4" src="<?php echo DIRIMG . "logo-sistema.png" ?>" alt="Auto Escola">
+        <h1 class="h3 mb-3 font-weight-normal">Faça login</h1>
+        <label for="inputEmail" class="sr-only">Endereço de email</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Seu email" required autofocus>
+        <label for="inputPassword" class="sr-only">Senha</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+        <div class="checkbox mb-3">
+            <label>
+                <input type="checkbox" value="remember-me"> Lembrar de mim
+            </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+    </form>
+
+
 </div>
 
 <?php
