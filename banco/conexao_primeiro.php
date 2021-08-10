b05efcd7398c9f46f0622719f20aae53c289302c
franciscocesar63 <franciscocesar888@gmail.com>
2021-08-10 16:08:13 -0300

    conexão

--- banco/conexao_primeiro.php
+++ banco/conexao_primeiro.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=u626289327_sysauto', "u626289327_sysauto", "Fr4xx$12");
+    $pdo = new PDO('mysql:host=localhost;dbname=u626289327_sysauto', "u626289327_sysauto", "");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
