30c87ed0fc1ba8876ce85d01ee3fcfb707d8d267
franciscocesar63 <franciscocesar888@gmail.com>
2021-08-10 15:30:25 -0300

    conexao com a hospedagem

--- banco/conexao_primeiro.php
+++ banco/conexao_primeiro.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=primeira', "root", "");
+    $pdo = new PDO('mysql:host=156.67.72.151;dbname=u626289327_sysauto', "u626289327_sysauto", "Fr4xx$12");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
